e8aaf7b26968a5d71e97e951e2941f7f62388daf
Owen Melbourne <owenmelbz@gmail.com>
2020-06-12 12:54:15 +0100

    🔨 WIP

--- bin/tunnel.sh
+++ bin/tunnel.sh
@@ -1,22 +1,34 @@
-#!/bin/bash
+#!/bin/sh
 
-echo ""
-echo "################################################"
-echo "## Welcome to Macron Tunnel -  SSH companion ##"
-echo "################################################"
-echo ""
-
+# shellcheck disable=SC2124
 SERVER="$@"
 
-echo "Forwarding the local port: 9876 to the remote port of: 9876."
-echo "GUI can be accessed on http://localhost:9876"
+# shellcheck disable=SC2039
+if [[ -z "${PORT}" ]]; then
+  SERVER_PORT=3322
+else
+  SERVER_PORT="${PORT}"
+fi
+
+echo "\033[0;32m";
 echo ""
+echo "################################################"
+echo "## Welcome to Macron Tunnel  -  SSH companion ##"
+echo "################################################"
+
+echo "\033[0;33m";
+echo "Forwarding the local port: $SERVER_PORT to the remote port of: $SERVER_PORT."
+echo "GUI can be accessed on http://localhost:$SERVER_PORT"
+echo ""
+if [ "$SERVER_PORT" == 3322 ]; then
+echo "If you want to change the port, define the PORT variable first e.g. PORT=9000 macrontunnel"
+echo ""
+fi
 echo "Remember to run 'macron' from the correct SSH user!"
-echo ""
+echo "\033[0m";
+COMMAND="ssh -L $SERVER_PORT:localhost:$SERVER_PORT $SERVER"
 
-COMMAND="ssh -L 9876:localhost:9876 $SERVER"
-
-echo $COMMAND
+echo "$COMMAND"
 echo ""
 
 $COMMAND
